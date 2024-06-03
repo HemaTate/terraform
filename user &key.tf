7a8177b7270308a754808922742a6aebdd248566
HemaTate <166596958+HemaTate@users.noreply.github.com>
2024-06-03 13:45:27 +0530

    Update user &key.tf

--- user &key.tf
+++ user &key.tf
@@ -23,7 +23,10 @@ resource "aws_iam_user_policy" "hm_ro" {
   user   = aws_iam_user.hm.name
   policy = data.aws_iam_policy_document.hm_ro.json
 }
-
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
 
 
 
