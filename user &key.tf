2de06d74e5342d1813f2e93488b4aec9294d0a27
HemaTate <166596958+HemaTate@users.noreply.github.com>
2024-06-03 13:27:41 +0530

    Update user &key.tf

--- user &key.tf
+++ user &key.tf
@@ -12,15 +12,16 @@ resource "aws_iam_access_key" "hm" {
   user = aws_iam_user.hm.name
 }
 data "aws_iam_policy_document" "hm_ro" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:Describe*"]
-    resources = ["*"]
-  }
+  statement  [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
 }
-
 resource "aws_iam_user_policy" "hm_ro" {
-  name   = "test"
+  name   = "administratorfullaccess"
   user   = aws_iam_user.hm.name
   policy = data.aws_iam_policy_document.hm_ro.json
 }
